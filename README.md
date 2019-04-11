7dbefbfc04162a393cfa4ff2b8d2338b4e799db6
Natosha <nmsuri28@gmail.com>
2019-04-11 14:16:20 -0500

    First Commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # landing-page-demo
+
+Live Version:
+https://nmsuri28.github.io/landing-page-demo/
+
+Description:
+Just a demo landing page 
+
+Motivation:
+I have been on a serious pirate kick as of late
+
+Contact Me:
+Natosha[@creativeHotMess](https://twitter.com/creativeHotMess) | https://www.linkedin.com/in/natosha-martin-676263165
+
---
+++ main.js
@@ -0,0 +1,13 @@
+$(document).ready(function (){
+    $('.header').height($(window).height());
+
+
+})
+
+$("#moreInfo").on("click", alertUser);   
+$("#submit").on("click", alertUser);
+    
+function alertUser(){
+        event.preventDefault();
+        alert("Oops! This page is just a demo. But if you look in the footer, I have attached some ways to get ahold of me :)");
+    }
